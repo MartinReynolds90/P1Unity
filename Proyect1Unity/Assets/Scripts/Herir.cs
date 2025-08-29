7e474792ba1bf093f981c7e18f349e56b9c542b6
MartinReynolds90 <martinreynolds2607@gmail.com>
2025-08-29 09:47:42 -0300

    Agregue enemigos
    agregue enemigos y el script herir

---
+++ Proyect1Unity/Assets/Scripts/Herir.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herir : MonoBehaviour
+{
+    //Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] float puntos = 5f;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            Debug.Log("PUNTOS DE DAÑO RERALIZADOS" + puntos);
+        }
+    }
+}
